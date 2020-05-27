5ebc4a9451d2d7484b10c9c7d868642ff01d9439
Jakub Kutka <jacobkutka@gmail.com>
2020-05-27 02:19:45 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+public class Controller {
+public TextField loginField;
+public PasswordField passwordField;
+public Label errorMessage;
+public Button btnLogin;
+
+    public void clickBtn(ActionEvent actionEvent) {
+
+    }
+}
